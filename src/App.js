f716a511922abeffce790274e786990b20ade175
Stein Korsveien <stein_korsveien@msn.com>
2022-09-01 11:27:02 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary"
 
 function App() {
   return (
      <div>
       <h1>Bookkeeper!</h1>
+       <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <ErrorBoundary>
+            <Link to="/invoices">Invoices</Link> |{" "}
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+           <Link to="/expenses">Expenses</Link>
+        </ErrorBoundary>
+
+      </nav>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
